1024fdc8a6e43c386616edc8eebdb457727881f6
Rajendraprasad <aravindrajendraprasa@kpmg.com>
2017-02-11 23:15:41 -0800

    added DELETE method to users/:user

--- server/api.v1.js
+++ server/api.v1.js
@@ -92,6 +92,24 @@ router.get('/users/:user',function(req, res){
         }
     });
 })
+router.delete('/users/:user',function(req, res){
+    var query = "MATCH (user: User) WHERE user.userName = \""+req.params.user+"\" DELETE user;"
+    console.log(query);
+    var tx = db.beginTransaction();
+        db.cypher(query, function (err, results) {
+        if (err) {
+            res.status(401)
+            res.send("message: oops we need to start over again");
+        }
+        else {
+            console.log("successfully executed query. Going for commit");
+            tx.commit(function (err) {
+                res.status(204);
+                res.send("message: successfully deleted");
+            });
+        }
+    });
+})
 
 //const url = require('url');
 //const config = require('../config/app.config');
