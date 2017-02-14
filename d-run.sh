11797493de89c536e0ea4955d77e3c33f47651be
Pearce, Nick <npearce@kpmg.com>
2017-02-14 00:50:58 -0800

    docker components updated - added neo4j image - d-run.sh now creates a docker stack running both webstart and neo4j containers - stack definition: docker-compose.yml (compose v3)

--- d-run.sh
+++ d-run.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
-docker run --rm -p 8080:8080 --name "web" webstart:3
+
+# run an isolated container with the webstart image
+# docker run --rm -p 8080:8080 --name "web" webstart:3
+
+# deploy a docker stack using a compose file (v3)
+docker deploy -c docker-compose.yml pqvp-stack
+echo
+echo webstart running on http://localhost:8080
+echo neo4m running on http://localhost:7474
+echo neo4j /data directory persisted at ./neo4j
+echo
