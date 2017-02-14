31c8e60b56bd9806f98030fe6085c35704053ca5
Robinson <christopherrobinson@kpmg.com>
2017-02-14 10:35:14 -0800

    Updates from pull request #72

--- client/e2e.js
+++ client/e2e.js
@@ -1,12 +1,12 @@
 //TODO: change to false for angular
 browser.ignoreSynchronization = true;
 
-describe(`sample test`, () => {
+describe('sample test', () => {
     beforeEach(done => {
         browser.get('/').then(done);
     });
 
-    it(`page should have a title`, () => {
+    it('page should have a title', () => {
         expect(browser.driver.getTitle()).toBe('Webstart3');
     });
 });
