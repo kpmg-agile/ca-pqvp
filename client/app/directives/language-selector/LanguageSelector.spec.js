466bd1744b15d567b4efb1fd8c57c1fe5edf55c0
robertlevy <robertlevy@kpmg.com>
2017-02-17 16:02:33 -0500

    got a test to pass

--- client/app/directives/language-selector/LanguageSelector.spec.js
+++ client/app/directives/language-selector/LanguageSelector.spec.js
@@ -1,49 +1,54 @@
-import {Component} from '@angular/core';
+import {async, TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {APP_DIRECTIVES} from '../../directives';
+import {APP_PIPES} from '../../pipes';
+import {APP_COMPONENTS} from '../../routes';
+import {APP_PROVIDERS} from '../../providers';
+import APP_IMPORTS from '../../imports';
+
 import LanguageSelector from './LanguageSelector';
-import {
-    addProviders,
-    async,
-    inject,
-    TestComponentBuilder,
-    ComponentFixture
-} from '@angular/core/testing';
 
-@Component({
-    selector: 'test-component',
-    directives: [LanguageSelector],
-    template: ''
-})
-class TestComponent {}
-
-//TODO: Enable tests by changing "xdescribe" to "describe"
-xdescribe('LanguageSelector', () => {
+describe('LanguageSelector', () => {
 
     beforeEach(() => {
-        addProviders([LanguageSelector]);
+        TestBed.configureTestingModule({
+            declarations: [ ...APP_DIRECTIVES, ...APP_PIPES, ...APP_COMPONENTS ],
+            providers: APP_PROVIDERS,
+            imports: [ ...APP_IMPORTS, RouterTestingModule ]
+        });
     });
 
-    it('should return component name', inject([LanguageSelector], (languageSelector:LanguageSelector) => {
-        expect(languageSelector.name).toBe('LanguageSelector');
+    it('should be creatable', async(() => {
+        TestBed.compileComponents().then(() => {
+            const fixture = TestBed.createComponent(LanguageSelector);
+            expect(fixture.componentInstance).toBeDefined();
+            expect(fixture.debugElement.nativeElement.innerHTML).toBeTruthy();
+        });
     }));
 
-    it('should initialize default name to heading', async(inject([TestComponentBuilder], (tcb:TestComponentBuilder) => {
-        return tcb
-            .overrideTemplate(TestComponent, '<language-selector></language-selector>')
-            .createAsync(TestComponent)
-            .then((fixture:ComponentFixture) => {
-                fixture.detectChanges();
-                expect(fixture.nativeElement.querySelector('language-selector h1').innerText).toBe('LanguageSelector');
-            });
-    })));
 
-    it('should initialize custom name to heading', async(inject([TestComponentBuilder], (tcb:TestComponentBuilder) => {
-        return tcb
-            .overrideTemplate(TestComponent, '<language-selector name="TEST"></language-selector>')
-            .createAsync(TestComponent)
-            .then((fixture:ComponentFixture) => {
-                fixture.detectChanges();
-                expect(fixture.nativeElement.querySelector('language-selector h1').innerText).toBe('TEST');
-            });
-    })));
+    // it('should return component name', inject([LanguageSelector], (languageSelector:LanguageSelector) => {
+    //     expect(languageSelector.name).toBe('LanguageSelector');
+    // }));
+
+    // it('should initialize default name to heading', async(inject([TestComponentBuilder], (tcb:TestComponentBuilder) => {
+    //     return tcb
+    //         .overrideTemplate(TestComponent, '<language-selector></language-selector>')
+    //         .createAsync(TestComponent)
+    //         .then((fixture:ComponentFixture) => {
+    //             fixture.detectChanges();
+    //             expect(fixture.nativeElement.querySelector('language-selector h1').innerText).toBe('LanguageSelector');
+    //         });
+    // })));
+
+    // it('should initialize custom name to heading', async(inject([TestComponentBuilder], (tcb:TestComponentBuilder) => {
+    //     return tcb
+    //         .overrideTemplate(TestComponent, '<language-selector name="TEST"></language-selector>')
+    //         .createAsync(TestComponent)
+    //         .then((fixture:ComponentFixture) => {
+    //             fixture.detectChanges();
+    //             expect(fixture.nativeElement.querySelector('language-selector h1').innerText).toBe('TEST');
+    //         });
+    // })));
 
 });
--- client/spec.js
+++ client/spec.js
@@ -6,6 +6,20 @@ import 'zone.js/dist/sync-test';
 import 'zone.js/dist/proxy';
 import 'zone.js/dist/jasmine-patch';
 
+import {BrowserDynamicTestingModule, platformBrowserDynamicTesting} from '@angular/platform-browser-dynamic/testing';
+import {TestBed, MockApplicationRef} from '@angular/core/testing';
+import {ApplicationRef} from '@angular/core';
+import {Http, BaseRequestOptions} from '@angular/http';
+import {MockBackend} from '@angular/http/testing';
+import {APP_PROVIDERS} from './app/providers';
+TestBed.initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting([
+    APP_PROVIDERS,
+    MockBackend,
+    BaseRequestOptions,
+    {provide: Http, useFactory: (backend, options) => { return new Http(backend, options); }, deps: [MockBackend, BaseRequestOptions]},
+    {provide: ApplicationRef, useClass: MockApplicationRef}
+]));
+
 //require all source files for proper coverage inspection
 const src = require.context('./', true, /[^spec|e2e]\.(js|jsx)$/);
 src.keys().forEach(src);
