63828f6bff33ba28d3093cc99f255d835010202b
robertlevy <robertlevy@kpmg.com>
2017-02-18 15:15:46 -0500

    overhaul of pseudoloc implementation

--- web_loaders/pseudoloc-loader.js
+++ web_loaders/pseudoloc-loader.js
@@ -1,11 +1,28 @@
-const vm = require('vm');
 const ploc = require('pseudoloc');
+const fs = require('fs');
+const path = require('path');
+const querystring = require('querystring');
 
-module.exports = function(source) {
+module.exports = function() {
 	this.cacheable && this.cacheable();
-	let value = typeof source === 'string' ? JSON.parse(source) : source;
-    Object.keys(value).forEach( (key) => {
-        value[key] = ploc.str(value[key]);
+
+    // get config values from the query parameter
+    let config = querystring.parse(this.query.replace('?', ''));
+
+    // load the strings to convert
+    const defaultStringsPath = path.resolve(__dirname,
+        `../client/locales/${config.lng}/${config.ns}.json`);
+    this.addDependency(defaultStringsPath);
+    let defaultStrings = fs.readFileSync(defaultStringsPath, 'utf-8');
+
+    defaultStrings = JSON.parse(defaultStrings);
+
+    // apply pseudoloc to each string
+    let pseudoStrings = {};
+    Object.keys(defaultStrings).forEach( (key) => {
+        pseudoStrings[key] = ploc.str(defaultStrings[key]);
     });
-    return JSON.stringify(value);
+
+    this.value = [pseudoStrings];
+    return 'module.exports=' + JSON.stringify(pseudoStrings);
 };
