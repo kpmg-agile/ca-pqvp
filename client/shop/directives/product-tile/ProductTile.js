351c979b7b41e74773f999818dd17c2ae6489cd3
robertlevy <robertlevy@kpmg.com>
2017-02-22 10:55:54 -0500

    fetch product image

--- client/shop/directives/product-tile/ProductTile.js
+++ client/shop/directives/product-tile/ProductTile.js
@@ -1,6 +1,8 @@
 import {Component, Input} from '@angular/core';
+import {DomSanitizer} from '@angular/platform-browser';
 import template from './ProductTile.html';
 import styles from './ProductTile.scss';
+import Api from '../../../../raml/api.v1.raml';
 
 @Component({
     selector: 'product-tile',
@@ -14,10 +16,27 @@ import styles from './ProductTile.scss';
  */
 export default class ProductTile {
 
+    _api:Api;
+    _sanitizer:DomSanitizer;
+    _product;;
+
     // product (based on the Product service schema)
-    @Input() product = null;
+    @Input()
+    get product() {
+        return this._product;
+    }
+    set product(value) {
+        this._product = value;
+        this.fetchImage();
+    }
 
-    constructor() {
+    async fetchImage() {
+        let image = await this._api.images.imageId({imageId: this.product.images[0]}).get().json();
+        this.primaryImage =  this._sanitizer.bypassSecurityTrustUrl(image.imageData);
+    }
 
+    constructor(sanitizer:DomSanitizer) {
+        this._sanitizer = sanitizer;
+        this._api = new Api();
     }
 }
