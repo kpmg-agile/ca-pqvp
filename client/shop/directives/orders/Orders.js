99d4b5dcafe122cbb5e94f13e363e22da3d5e223
robertlevy <robertlevy@kpmg.com>
2017-02-23 14:58:50 -0500

    orders page functionality

--- client/shop/directives/orders/Orders.js
+++ client/shop/directives/orders/Orders.js
@@ -1,6 +1,9 @@
-import {Component, Input, Output, EventEmitter} from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {DomSanitizer} from '@angular/platform-browser';
+
 import template from './Orders.html';
 import styles from './Orders.scss';
+import Api from '../../../../raml/api.v1.raml';
 
 @Component({
     selector: 'orders',
@@ -19,13 +22,45 @@ export default class Orders {
      */
     @Input() name:string = 'Orders';
 
-    /**
-     * An example output for this component
-     * @see https://angular.io/docs/ts/latest/api/core/Output-var.html
-     */
-    @Output() change:EventEmitter = new EventEmitter();
+    _api:Api;
+    _sanitizer:DomSanitizer;
+    groupedOrders:Array;
+    currentOrder:Object;
 
-    constructor() {
+    constructor(sanitizer:DomSanitizer) {
+        this._sanitizer = sanitizer;
+        this._api = new Api();
+    }
 
+    async ngOnInit() {
+        let orders = await this._api.orders.get().json();
+        this.groupedOrders = [
+            {key: 'inProgress', orders: orders.filter( o => o.status === 'PROCESSING' )},
+            {key: 'completed', orders: orders.filter( o => o.status === 'COMPLETE' )},
+            {key: 'closedOrCancelled', orders: orders.filter( o => o.status === 'CLOSED' )}
+        ];
+
+        this.currentGroup = 'inProgress';
+        if (orders.length) {
+            this.selectOrder(orders[0]);
+        }
+    }
+
+    async selectOrder(order) {
+        this.currentOrder = order;
+        order.orderItems.forEach( item => { this.loadItemDetails(item); } );
+    }
+
+    async loadItemDetails(item) {
+        let itemDetails = await this._api.products.productId({productId: item.orderItemId}).get().json();
+        item.name = itemDetails.name;
+        item.unitPrice = itemDetails.unitPrice;
+
+        let image = await this._api.images.imageId({imageId: itemDetails.images[0]}).get().json();
+        item.image =  this._sanitizer.bypassSecurityTrustUrl(image.imageData);
+    }
+
+    cancelOrder(/*order*/) {
+        alert('Not yet implemented'); // eslint-disable-line
     }
 }
