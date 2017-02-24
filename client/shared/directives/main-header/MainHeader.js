9d78f429454b0a18688edee4c5cb9445da04cd04
Casey Rayl <crayl@kpmg.com>
2017-02-23 19:55:43 -0700

    Adding animations into the mobile menu states.

--- client/shared/directives/main-header/MainHeader.js
+++ client/shared/directives/main-header/MainHeader.js
@@ -1,15 +1,36 @@
-import {Component} from '@angular/core';
+import {Component, trigger, state, style, transition, animate} from '@angular/core';
 import template from './MainHeader.html';
 import styles from './MainHeader.scss';
 import Api from '../../../../raml/api.v1.raml';
 import {Router} from '@angular/router';
 import {CartService} from '../../../app/providers';
 
-
 @Component({
     selector: 'main-header',
     template: template,
-    styles: [styles]
+    styles: [styles],
+    animations: [
+        trigger('blockerVisible', [
+            state('open', style({
+                opacity: '1'
+            })),
+            state('closed', style({
+                opacity: '0'
+            })),
+            transition('open => closed', animate('250ms ease-in')),
+            transition('closed => open', animate('250ms ease-out'))
+        ]),
+        trigger('menuSlide', [
+            state('open', style({
+                right: '0px'
+            })),
+            state('closed', style({
+                right: '-230px'
+            })),
+            transition('open => closed', animate('250ms ease-in')),
+            transition('closed => open', animate('250ms ease-out'))
+        ])
+    ]
 })
 /**
  * @see https://angular.io/docs/ts/latest/api/core/Component-decorator.html
@@ -18,8 +39,12 @@ import {CartService} from '../../../app/providers';
  */
 export default class MainHeader {
 
+    CLOSED_STATE:String = 'closed';
+    OPEN_STATE:String = 'open';
+
     cartService:CartService;
     isSlideMenuOpen:Boolean = false;
+    menuAnimationState:String = this.CLOSED_STATE;
 
     constructor(router:Router, cartService:CartService) {
         this.router = router;
@@ -38,9 +63,16 @@ export default class MainHeader {
 
     openMenu() {
         this.isSlideMenuOpen = true;
+        this.menuAnimationState = this.OPEN_STATE;
     }
 
     closeMenu() {
-        this.isSlideMenuOpen = false;        
+        this.menuAnimationState = this.CLOSED_STATE;
+    }
+
+    onCloseComplete() {
+        if (this.menuAnimationState === this.CLOSED_STATE) {
+            this.isSlideMenuOpen = false;
+        }
     }
 }
