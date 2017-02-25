9cef4db20d27b777910d39619451db3b383f3229
Pearce, Nick <npearce@kpmg.com>
2017-02-25 05:56:46 -0800

    Added Expenditure data to database (committing graph.db in stopped state) Data import - Added PATCH /api/v1/expenditures - PATCH seemed appropriate enough but we could alternatively create custom POST routes - PATCH will import data defined in expenditure-collection.examples.json - Feels a bit hacky but it seems like a straightforward method for seeding the database while in dev. It also guarantees that the data returned will be identical to the mocks. - expenditure-collection.example.json has been populated with data to build the dashboard charts (whether invoked from mocks or db) Refactoring - moved expenditure api functions to ./api.v1.expenditures.js. - not sure of best pattern for require() when we have to combine functions on same router object. Alternative would be to update api.js to look more broadly for api.v1.*.js files - refactoring not complete - expenditures includes duplicate common functions

---
+++ server/api.v1.expenditures.js
@@ -0,0 +1,179 @@
+const Router = require('express').Router;
+const router = new Router();
+const neo4j = require('neo4j');
+const fs = require('fs');
+const dbconnection = JSON.parse(fs.readFileSync('config/.dbconfig', 'utf8'));
+const tosource = require('tosource');
+const _ = require('lodash');
+const db = new neo4j.GraphDatabase('http://' + dbconnection.dbaccount + ':' + dbconnection.dbpassword + '@' + dbconnection.dblocation);
+
+// Expenditures
+
+router.get('/api/v1/expenditures', function(req, res){
+    let params = {};
+    let query = 'MATCH (expenditure:Expenditure) RETURN expenditure';
+
+    let collectionQuery = buildCollectionQuery(req.query);
+    params = _.extend(params, collectionQuery.queryParams);
+    query += collectionQuery.queryString;
+
+    getQuery(query, params, res, false, function(row){
+        return row.properties;
+    });
+});
+
+router.get('/api/v1/expenditures/byyear', function(req, res){
+    let params = {};
+    let query = 'MATCH (expenditure:Expenditure) RETURN expenditure';
+
+    let collectionQuery = buildCollectionQuery(req.query);
+    params = _.extend(params, collectionQuery.queryParams);
+    query += collectionQuery.queryString;
+
+    getQuery(query, params, res, false, function(row){
+        return row.properties;
+    });
+});
+
+router.post('/api/v1/expenditures', function (req, res) {
+    let expenditure = req.body;
+
+    let query = 'CREATE (expenditure:Expenditure ' + tosource(expenditure) + ') RETURN expenditure;';
+    postQuery(query, expenditure)
+        .then(result => {
+            sendResult(res, result);
+        })
+        .catch(error => {
+            sendError(res, error);
+        });
+});
+
+router.patch('/api/v1/expenditures', function (req, res) {
+    const props = require('../raml/api/v1/expenditures/expenditure-collection.example.json');
+    let params = {
+        props: props
+    };
+    let query = 'UNWIND $props AS map CREATE (expenditure:Expenditure) SET expenditure = map';
+    patchQuery(query, params, res);
+});
+
+// Supporting Functions
+
+function sendResult(response, result) {
+    response.status(result.status);
+    response.send(result.send);
+}
+
+function sendError(response, errorDef) {
+    console.log('sendError: ', errorDef);
+    if (errorDef.message) {
+        errorDef.send = JSON.stringify({message: errorDef.message});
+    }
+    response.status(errorDef.status);
+    response.send(errorDef.send);
+}
+
+function buildCollectionQuery(requestParams) {
+    let queryString = '';
+    let queryParams = {};
+    if (requestParams.sortDescending && requestParams.sortDescending === 'true') {
+        queryString += ' DESC';
+    }
+    if (requestParams.page && requestParams.page > 0) {
+        queryParams.skippage = (requestParams.page - 1) * requestParams.pageSize;
+        queryString += ' SKIP {skippage}';
+    }
+    if (requestParams.pageSize) {
+        queryParams.limit = requestParams.pageSize;
+        queryString += ' LIMIT {limit}';
+    }
+    return {queryString, queryParams};
+}
+
+function getQuery(query, params, res, singleEntity, mapper) {
+
+    // default to the identity function
+    mapper = mapper || function (x) {
+            return x;
+        };
+
+    let tx = db.beginTransaction();
+    db.cypher({query, params}, function callback(err, results) {
+        if (err) {
+            console.log(query);
+            console.log(err);
+            sendError(res, {status: 409, send: ''});
+        }
+        else {
+            console.log('successfully executed query. Going for commit');
+            tx.commit(function (err) {
+                if (err) {
+                    sendError(res, {status: 409, send: ''});
+                }
+                else {
+                    results = results.map(r => mapper(_.values(r)[0]));
+                    if (singleEntity) {
+                        results = results.length ? results[0] : {};
+                    }
+                    sendResult(res, {status: 201, send: JSON.stringify(results)});
+                }
+            });
+        }
+    });
+}
+
+function patchQuery(query, params, res) {
+
+    let tx = db.beginTransaction();
+    db.cypher({query, params}, function (err) {
+        if (err) {
+            console.log(query);
+            console.log(err);
+            sendError(res, {status: 409, send: ''});
+        }
+        else {
+            console.log('successfully executed query. Going for commit');
+            tx.commit(function (err) {
+                if (err) {
+                    sendError(res, {status: 409, send: ''});
+                }
+                else {
+                    sendResult(res, {status: 201, send: {patch: 'success'}});
+                }
+            });
+        }
+    });
+}
+
+function postQuery(query, property) {
+
+    let responseJSON = {};
+    let tx = db.beginTransaction();
+    return new Promise(function (resolve, reject) {
+        db.cypher(query, function (err) {
+            if (err) {
+                console.log(err);
+                responseJSON.status = 409;
+                responseJSON.send = '';
+                reject(responseJSON);
+            }
+            else {
+                console.log('successfully executed query. Going for commit');
+                tx.commit(function (err) {
+                    if (err) {
+                        responseJSON.status = 409;
+                        responseJSON.send = '';
+                        reject(responseJSON);
+                    }
+                    else {
+                        responseJSON.status = 201;
+                        responseJSON.send = JSON.stringify(property);
+                        resolve(responseJSON);
+                    }
+                });
+            }
+        });
+    });
+}
+
+module.exports = router;
--- server/api.v1.js
+++ server/api.v1.js
@@ -3,12 +3,15 @@ const router = new Router();
 const neo4j = require('neo4j');
 const fs = require('fs');
 const dbconnection = JSON.parse(fs.readFileSync('config/.dbconfig', 'utf8'));
-// const tosource = require('tosource');
+//const tosource = require('tosource');
 const _ = require('lodash');
 const db = new neo4j.GraphDatabase('http://' + dbconnection.dbaccount + ':' + dbconnection.dbpassword + '@' + dbconnection.dblocation);
 const jwt = require('jsonwebtoken');
 const appConfig = require('../config/app.config');
 const authConfig = require('../config/auth.config');
+
+// What's the preferred exports pattern?
+_.extend(router, require('./api.v1.expenditures.js'));
 
 /**
  * Login
@@ -16,7 +19,7 @@ const authConfig = require('../config/auth.config');
 router.post('/api/v1/login', function (req, res) {
     let credentials = req.body;
     let query = 'MATCH (user:User {userName:{username}, password:{password}}) RETURN {firstName: user.firstName, lastName: user.lastName, userName: user.userName, userId: user.userId };';
-    let params = { username: credentials.userName, password: credentials.password };
+    let params = {username: credentials.userName, password: credentials.password};
     authenticate(req, res, query, params);
 });
 
@@ -67,7 +70,7 @@ router.get('/api/v1/users', function (req, res) {
 //             sendError(res, error);
 //         });
 // });
-
+//
 // router.delete('/api/v1/users/:user', function (req, res) {
 //     let userName = req.params.user;
 //     let query = 'MATCH (user:User {userName: {name}}) DELETE user;';
@@ -80,9 +83,9 @@ router.get('/api/v1/users', function (req, res) {
 //             sendError(res, error);
 //         });
 // });
-
+//
 // Products
-
+//
 // router.post('/api/v1/products', function (req, res) {
 //     let product = req.body;
 //     let items = product.images;
@@ -98,11 +101,11 @@ router.get('/api/v1/users', function (req, res) {
 // });
 
 function productMapper(row) {
-        let consolidatedRow = row.product.properties;
-        consolidatedRow.productId = row.product._id;
-        consolidatedRow.images = row.imageIds;
-        consolidatedRow.defaultImageId = row.imageIds.length ? row.imageIds[0] : null;
-        return consolidatedRow;
+    let consolidatedRow = row.product.properties;
+    consolidatedRow.productId = row.product._id;
+    consolidatedRow.images = row.imageIds;
+    consolidatedRow.defaultImageId = row.imageIds.length ? row.imageIds[0] : null;
+    return consolidatedRow;
 }
 
 router.get('/api/v1/products', function (req, res) {
@@ -138,12 +141,9 @@ router.get('/api/v1/products/:product', function (req, res) {
              MATCH (product)-[:hasImage]->(image:Image) \
              RETURN { product:product, imageIds:collect(ID(image)) }';
 
-    params = { productId: parseInt(req.params.product, 10) };
+    params = {productId: parseInt(req.params.product, 10)};
     getQuery(query, params, res, true, productMapper);
 });
-
-
-
 
 // router.delete('/api/v1/products/:product', function (req, res) {
 
@@ -157,12 +157,12 @@ router.get('/api/v1/products/:product', function (req, res) {
 //             sendError(res, error);
 //         });
 // });
-
-// Images
-
+//
+//// Images
+//
 // router.post('/api/v1/images', function (req, res) {
 //     let image = req.body;
-
+//
 //     let query = 'CREATE (image:Image ' + tosource(image) + ') RETURN image;';
 //     postQuery(query, image)
 //         .then(result => {
@@ -171,17 +171,17 @@ router.get('/api/v1/products/:product', function (req, res) {
 //         .catch(error => {
 //             sendError(res, error);
 //         });
-
+//
 // });
-
+//
 // router.get('/api/v1/images', function (req, res) {
 //     let params = {};
 //     let query = 'MATCH (image: Image) return image';
-
+//
 //     let collectionQuery = buildCollectionQuery(req.query);
 //     params = _.extend(params, collectionQuery.queryParams);
 //     query += collectionQuery.queryString;
-
+//
 //     getQuery(query, params, 'image', 'imageId')
 //         .then(result => {
 //             sendResult(res, result);
@@ -189,166 +189,203 @@ router.get('/api/v1/products/:product', function (req, res) {
 //         .catch(error => {
 //             sendError(res, error);
 //         });
-
+//
 // });
 
 router.get('/api/v1/images/:image', function (req, res) {
 
     let query = 'MATCH (image: Image) where ID(image)={imageid} RETURN image;';
-    let params = { imageid: parseInt(req.params.image, 10) };
-    getQuery(query, params, res, true, image => image.properties );
+    let params = {imageid: parseInt(req.params.image, 10)};
+    getQuery(query, params, res, true, image => image.properties);
 });
 
-// router.delete('/api/v1/images/:image', function (req, res) {
-//     let query = 'MATCH (image: Image) where ID(image)={imageid} DETACH DELETE image;';
-//     let params = { imageid: parseInt(req.params.image,10) };
-//     deleteQuery(query, params)
-//         .then(result => {
-//             sendResult(res, result);
-//         })
-//         .catch(error => {
-//             sendError(res, error);
-//         });
+// // router.delete('/api/v1/images/:image', function (req, res) {
+// //     let query = 'MATCH (image: Image) where ID(image)={imageid} DETACH DELETE image;';
+// //     let params = { imageid: parseInt(req.params.image,10) };
+// //     deleteQuery(query, params)
+// //         .then(result => {
+// //             sendResult(res, result);
+// //         })
+// //         .catch(error => {
+// //             sendError(res, error);
+// //         });
+//
+// // });
+//
+// // OrderItems
+//
+// // router.get('/api/v1/order-items', function (req, res) {
+// //     let params = {};
+// //     let query = 'MATCH (orderitems: OrderItems) return orderitems';
+//
+// //     let collectionQuery = buildCollectionQuery(req.query);
+// //     params = _.extend(params, collectionQuery.queryParams);
+// //     query += collectionQuery.queryString;
+//
+// //     getQuery(query, params, 'orderitems.properties')
+// //         .then(result => {
+// //             sendResult(res, result);
+// //         })
+// //         .catch(error => {
+// //             sendError(res, error);
+// //         });
+// // });
+//
+// // router.get('/api/v1/order-items/:orderitems', function (req, res) {
+// //     let query = 'MATCH (orderitems: OrderItem orderItemId:{orderitemid}}) RETURN orderitems;';
+// //     let params = { orderitemid: req.params.orderitems };
+// //     getQuery(query, params, 'orderitems', 'orderItemId', true)
+// //         .then(result => {
+// //             sendResult(res, result);
+// //         })
+// //         .catch(error => {
+// //             sendError(res, error);
+// //         });
+//
+// // });
+//
+// // router.delete('/api/v1/order-items/:orderitems', function (req, res) {
+// //     let query = 'MATCH (orderitems: OrderItem orderItemId:{orderitemid}}) DETACH DELETE orderitems;';
+// //     let params = { orderitemid: req.params.orderitems };
+// //     deleteQuery(query, params)
+// //         .then(result => {
+// //             sendResult(res, result);
+// //         })
+// //         .catch(error => {
+// //             sendError(res, error);
+// //         });
+//
+// // });
+//
+// // Orders
+//
+// /*router.post('/api/v1/orders/current/add-item', function (req, res) {
+//  let orders = req.body;
+//  let tx = db.beginTransaction();
+//  if (Number.isInteger(orders.orderId) && checkforspecialcharacters(orders.dateCreated))
+//  {
+//  let items=orders.orderItems;
+//  delete(orders.orderItems);
+//  let listOfOrderItemIds=[];
+//  for (let i=0; i<items.length; i++)
+//  {
+//  listOfOrderItemIds.push(items[i].orderItemId);
+//  }
+//  let query = 'CREATE (orderitems:OrderItems ' + tosource(items) + ') CREATE (orders:Orders ' + tosource(orders) + ') WITH orders MATCH(o:OrderItems) where o.orderItemId in '+ tosource(listOfOrderItemIds) + ' Create(orders)-[:hasOrders]->(i)';
+//  console.log(query);
+//  db.cypher(query, function (err) {
+//  if (err) {
+//  res.status(409);
+//  res.send();
+//  }
+//  else {
+//  console.log('successfully executed query. Going for commit');
+//  tx.commit(function (err) {
+//  if (err)
+//  {
+//  res.status(409);
+//  res.send();
+//  }
+//  else
+//  {
+//  res.status(201);
+//  res.send(JSON.stringify(orders));
+//  }
+//  });
+//  }
+//  });
+//  }
+//  else
+//  {
+//  res.status(401);
+//  res.send('{\'message\':\'Invalid Request\'}');
+//  }
+//  });*/
+//
+// // router.get('/api/v1/orders', function (req, res) {
+// //     let params = {};
+// //     let query = 'MATCH (orders: Orders) return orders';
+//
+// //     let collectionQuery = buildCollectionQuery(req.query);
+// //     params = _.extend(params, collectionQuery.queryParams);
+// //     query += collectionQuery.queryString;
+//
+// //     getQuery(query, params, 'orders', 'orderId')
+// //         .then(result => {
+// //             sendResult(res, result);
+// //         })
+// //         .catch(error => {
+// //             sendError(res, error);
+// //         });
+//
+// // });
+//
+// // router.get('/api/v1/orders/:orders', function (req, res) {
+// //     let query = 'MATCH (orders: Orders {orderId: {orderid}}) RETURN orders;';
+// //     let params = { orderid: req.params.orders };
+// //     getQuery(query, params, 'orders', 'orderId', true)
+// //         .then(result => {
+// //             sendResult(res, result);
+// //         })
+// //         .catch(error => {
+// //             sendError(res, error);
+// //         });
+//
+// // });
+//
+// // router.delete('/api/v1/orders/:orders', function (req, res) {
+// //     let query = 'MATCH (orders: Orders {orderId: {orderid}}) DETACH DELETE orders;';
+// //     let params = { orderid: req.params.orders };
+// //     deleteQuery(query, params)
+// //         .then(result => {
+// //             sendResult(res, result);
+// //         })
+// //         .catch(error => {
+// //             sendError(res, error);
+// //         });
+//
+// // });
 
-// });
+/**
+ * Authenticate with the database user record using the supplied credentials
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ * @param {string} query - Database query
+ * @param {string} params - Database query parameters
+ * @return {undefined}
+ */
+function authenticate(req, res, query, params) {
+    let responseDef;
+    let tx = db.beginTransaction();
 
-// OrderItems
+    db.cypher({query, params}, function (err, results) {
+        if (!err) {
+            results = results.map(r => _.values(r)[0]);
+            console.log(JSON.stringify(results));
+            tx.commit(function (err) {
+                if (!err && results.length > 0) {
+                    let user = results[0];
 
-// router.get('/api/v1/order-items', function (req, res) {
-//     let params = {};
-//     let query = 'MATCH (orderitems: OrderItems) return orderitems';
+                    // Attach set-cookie to the response with token
+                    const token = jwt.sign(user, authConfig.secret);
+                    res.cookie(appConfig.authentication.cookieName, token);
 
-//     let collectionQuery = buildCollectionQuery(req.query);
-//     params = _.extend(params, collectionQuery.queryParams);
-//     query += collectionQuery.queryString;
+                    responseDef = {status: 201, send: JSON.stringify(user)};
+                }
+            });
+        }
 
-//     getQuery(query, params, 'orderitems.properties')
-//         .then(result => {
-//             sendResult(res, result);
-//         })
-//         .catch(error => {
-//             sendError(res, error);
-//         });
-// });
-
-// router.get('/api/v1/order-items/:orderitems', function (req, res) {
-//     let query = 'MATCH (orderitems: OrderItem orderItemId:{orderitemid}}) RETURN orderitems;';
-//     let params = { orderitemid: req.params.orderitems };
-//     getQuery(query, params, 'orderitems', 'orderItemId', true)
-//         .then(result => {
-//             sendResult(res, result);
-//         })
-//         .catch(error => {
-//             sendError(res, error);
-//         });
-
-// });
-
-// router.delete('/api/v1/order-items/:orderitems', function (req, res) {
-//     let query = 'MATCH (orderitems: OrderItem orderItemId:{orderitemid}}) DETACH DELETE orderitems;';
-//     let params = { orderitemid: req.params.orderitems };
-//     deleteQuery(query, params)
-//         .then(result => {
-//             sendResult(res, result);
-//         })
-//         .catch(error => {
-//             sendError(res, error);
-//         });
-
-// });
-
-// Orders
-
-/*router.post('/api/v1/orders/current/add-item', function (req, res) {
- let orders = req.body;
- let tx = db.beginTransaction();
- if (Number.isInteger(orders.orderId) && checkforspecialcharacters(orders.dateCreated))
- {
- let items=orders.orderItems;
- delete(orders.orderItems);
- let listOfOrderItemIds=[];
- for (let i=0; i<items.length; i++)
- {
- listOfOrderItemIds.push(items[i].orderItemId);
- }
- let query = 'CREATE (orderitems:OrderItems ' + tosource(items) + ') CREATE (orders:Orders ' + tosource(orders) + ') WITH orders MATCH(o:OrderItems) where o.orderItemId in '+ tosource(listOfOrderItemIds) + ' Create(orders)-[:hasOrders]->(i)';
- console.log(query);
- db.cypher(query, function (err) {
- if (err) {
- res.status(409);
- res.send();
- }
- else {
- console.log('successfully executed query. Going for commit');
- tx.commit(function (err) {
- if (err)
- {
- res.status(409);
- res.send();
- }
- else
- {
- res.status(201);
- res.send(JSON.stringify(orders));
- }
- });
- }
- });
- }
- else
- {
- res.status(401);
- res.send('{\'message\':\'Invalid Request\'}');
- }
- });*/
-
-// router.get('/api/v1/orders', function (req, res) {
-//     let params = {};
-//     let query = 'MATCH (orders: Orders) return orders';
-
-//     let collectionQuery = buildCollectionQuery(req.query);
-//     params = _.extend(params, collectionQuery.queryParams);
-//     query += collectionQuery.queryString;
-
-//     getQuery(query, params, 'orders', 'orderId')
-//         .then(result => {
-//             sendResult(res, result);
-//         })
-//         .catch(error => {
-//             sendError(res, error);
-//         });
-
-// });
-
-// router.get('/api/v1/orders/:orders', function (req, res) {
-//     let query = 'MATCH (orders: Orders {orderId: {orderid}}) RETURN orders;';
-//     let params = { orderid: req.params.orders };
-//     getQuery(query, params, 'orders', 'orderId', true)
-//         .then(result => {
-//             sendResult(res, result);
-//         })
-//         .catch(error => {
-//             sendError(res, error);
-//         });
-
-// });
-
-// router.delete('/api/v1/orders/:orders', function (req, res) {
-//     let query = 'MATCH (orders: Orders {orderId: {orderid}}) DETACH DELETE orders;';
-//     let params = { orderid: req.params.orders };
-//     deleteQuery(query, params)
-//         .then(result => {
-//             sendResult(res, result);
-//         })
-//         .catch(error => {
-//             sendError(res, error);
-//         });
-
-// });
-
-// Supporting Functions
+        if (responseDef) {
+            sendResult(res, responseDef);
+        }
+        else {
+            sendError(res, {status: 401, message: 'Invalid username or password'});
+        }
+    });
+}
 
 function sendResult(response, result) {
+
     response.status(result.status);
     response.send(result.send);
 }
@@ -356,7 +393,7 @@ function sendResult(response, result) {
 function sendError(response, errorDef) {
     console.log('sendError: ', errorDef);
     if (errorDef.message) {
-        errorDef.send = JSON.stringify({ message: errorDef.message });
+        errorDef.send = JSON.stringify({message: errorDef.message});
     }
     response.status(errorDef.status);
     response.send(errorDef.send);
@@ -376,74 +413,39 @@ function buildCollectionQuery(requestParams) {
         queryParams.limit = requestParams.pageSize;
         queryString += ' LIMIT {limit}';
     }
-    return { queryString, queryParams };
-}
-
-/**
- * Authenticate with the database user record using the supplied credentials
- * @param {Object} req - Express request object
- * @param {Object} res - Express response object
- * @param {string} query - Database query
- * @param {string} params - Database query parameters
- * @return {undefined}
- */
-function authenticate(req, res, query, params) {
-    let responseDef;
-    let tx = db.beginTransaction();
-
-    db.cypher({ query, params }, function (err, results) {
-        if (!err) {
-            results = results.map( r => _.values(r)[0]);
-            console.log(JSON.stringify(results));
-            tx.commit(function (err) {
-                if (!err && results.length > 0) {
-                    let user = results[0];
-
-                    // Attach set-cookie to the response with token
-                    const token = jwt.sign(user, authConfig.secret);
-                    res.cookie(appConfig.authentication.cookieName, token);
-
-                    responseDef = { status: 201, send: JSON.stringify(user) };
-                }
-            });
-        }
-
-        if (responseDef) {
-            sendResult(res, responseDef);
-        } else {
-            sendError(res, { status: 401, message: 'Invalid username or password' } );
-        }
-    });
+    return {queryString, queryParams};
 }
 
 function getQuery(query, params, res, singleEntity, mapper) {
 
     // default to the identity function
-    mapper = mapper || function (x) { return x; };
+    mapper = mapper || function (x) {
+            return x;
+        };
 
     let tx = db.beginTransaction();
-        db.cypher({ query, params }, function callback(err, results) {
-            if (err) {
-                console.log(query);
-                console.log(err);
-                sendError(res, {status: 409, send: ''});
-            }
-            else {
-                console.log('successfully executed query. Going for commit');
-                tx.commit(function (err) {
-                    if (err) {
-                        sendError(res, {status: 409, send: ''});
+    db.cypher({query, params}, function callback(err, results) {
+        if (err) {
+            console.log(query);
+            console.log(err);
+            sendError(res, {status: 409, send: ''});
+        }
+        else {
+            console.log('successfully executed query. Going for commit');
+            tx.commit(function (err) {
+                if (err) {
+                    sendError(res, {status: 409, send: ''});
+                }
+                else {
+                    results = results.map(r => mapper(_.values(r)[0]));
+                    if (singleEntity) {
+                        results = results.length ? results[0] : {};
                     }
-                    else {
-                        results = results.map( r => mapper(_.values(r)[0] ));
-                        if (singleEntity) {
-                            results = results.length ? results[0] : {};
-                        }
-                        sendResult(res, {status: 201, send: JSON.stringify(results)});
-                    }
-                });
-            }
-        });
+                    sendResult(res, {status: 201, send: JSON.stringify(results)});
+                }
+            });
+        }
+    });
 }
 
 // function deleteQuery(query, params) {
@@ -476,7 +478,7 @@ function getQuery(query, params, res, singleEntity, mapper) {
 // }
 
 // function postQuery(query, property) {
-
+//
 //     let responseJSON = {};
 //     let tx = db.beginTransaction();
 //     return new Promise(function (resolve, reject) {
