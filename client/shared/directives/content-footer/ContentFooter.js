9f0aa133adcc65a118382d6c84496d6b99f9d3df
Casey Rayl <crayl@kpmg.com>
2017-02-28 13:03:39 -0700

    Updating the focus state during the return to top action.

--- client/shared/directives/content-footer/ContentFooter.js
+++ client/shared/directives/content-footer/ContentFooter.js
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import template from './ContentFooter.html';
 import styles from './ContentFooter.scss';
+import $ from 'jquery';
 
 @Component({
     selector: 'content-footer',
@@ -19,6 +20,12 @@ export default class ContentFooter {
     }
 
     returnToTop() {
+        // using some script to target the top of the page because angular wants to navigate with an href="#top"
+        // scroll the window
         window.scrollTo(0,0);
+
+        // shift the focus onto the header we know should be there
+        let focusTarget = $('#languageOptions');
+        focusTarget.focus();
     }
 }
