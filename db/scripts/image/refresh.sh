d1a9fa69723211afecf3ea1633742e1f4feaa1ac
robertlevy <robertlevy@kpmg.com>
2017-02-28 21:42:39 -0500

    corrections

--- db/scripts/image/refresh.sh
+++ db/scripts/image/refresh.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-/var/lib/neo4j/bin/cypher-shell -u api -p api < /var/lib/neo4j/scripts/LoadCSV.cql
+/var/lib/neo4j/bin/cypher-shell -u neo4j -p calproc < /var/lib/neo4j/scripts/LoadCSV.cql
