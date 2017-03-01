4046251f5d85750954694dafff7ec55da9fc6a6f
Pearce, Nick <npearce@kpmg.com>
2017-02-28 19:02:41 -0800

    Fixed docker-db-refresh added docker-dev-init

--- db/scripts/local/set-neo4j-password.sh
+++ db/scripts/local/set-neo4j-password.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env bash
-# not used - for reference
-neo4j-admin set-initial-password calproc
-neo4j start
 curl -H "Content-Type: application/json" -X POST -d '{"password":"calproc"}' -u neo4j:neo4j http://localhost:7474/user/neo4j/password
